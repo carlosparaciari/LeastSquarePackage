e8182f55bee93136bf95c5cf4505c4cc739d8717
carlosparaciari <carlo.sparaciari.14@ucl.ac.uk>
2017-03-22 19:06:22 +0000

    Pop_point function for second vector implemented. Now test passes.

--- Code/Lib/lsqLeastSquare3D.cpp
+++ Code/Lib/lsqLeastSquare3D.cpp
@@ -22,21 +22,35 @@ namespace lsq {
   /// Method to pop back the last 3D point of the first vector.
   Eigen::Array3d LeastSquare3D::pop_point_first_vector() {
 
-    Eigen::Array3d last_element;
-
-    if( m_first_point_vector.empty() )
+    if( m_first_point_vector.empty() ) {
       throw std::length_error(m_lenght_error_message);
-    else
+    }
+    else {
+      Eigen::Array3d last_element;
       last_element = m_first_point_vector.back();
       m_first_point_vector.pop_back();
       return last_element;
+    }
+
   }
 
   /// Method to add a 3D point into the second vector.
   void LeastSquare3D::add_point_second_vector(const Eigen::Array3d & point) {}
 
   /// Method to pop back the last 3D point of the second vector.
-  Eigen::Array3d LeastSquare3D::pop_point_second_vector() {}
+  Eigen::Array3d LeastSquare3D::pop_point_second_vector() {
+
+    if( m_second_point_vector.empty() ) {
+      throw std::length_error(m_lenght_error_message);
+    }
+    else {
+      Eigen::Array3d last_element;
+      last_element = m_second_point_vector.back();
+      m_second_point_vector.pop_back();
+      return last_element;
+    }
+
+  }
 
 
 } // end namespace
