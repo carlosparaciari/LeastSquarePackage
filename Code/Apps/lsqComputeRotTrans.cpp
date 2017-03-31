1a72f50df0ee4d80e549c6d2335c152d05a8ad8e
carlosparaciari <carlo.sparaciari.14@ucl.ac.uk>
2017-03-31 09:08:08 +0100

    Command line application completed. Need to add the print to file part.

--- Code/Apps/lsqComputeRotTrans.cpp
+++ Code/Apps/lsqComputeRotTrans.cpp
@@ -15,10 +15,11 @@
 #include <boost/program_options.hpp>
 #include "lsqLeastSquare3D.h"
 #include <iostream>
+#include <fstream>
 #include <algorithm>
 #include <iterator>
 
-// A helper function to simplify the main part.
+// A helper function to simplify the help command.
 template<class T>
 std::ostream& operator<<(std::ostream& os, const std::vector<T>& v)
 {
@@ -71,31 +72,94 @@ int main(int argc, char* argv[]) {
       return 0;
     }
 
+    lsq::LeastSquare3D reference_frames;
+
     if (vm.count("input-files")) {
+
       auto input_filenames = vm["input-files"].as<std::vector<std::string>>();
+
       if (input_filenames.size() != 2) {
         std::cerr << "Provide two files with the sets of coordinates of the 3D points." << std::endl;
         return 1;
       }
-      std::cout << "Input files are: " << input_filenames << std::endl;
+
+      // ---------------------------- Read the coordinates and fill the two sets of 3D points ---------------------------
+      std::fstream filein;
+      double x_value, y_value, z_value;
+      Eigen::Vector3d point;
+
+      for( int i = 0 ; i < 2 ;  ++i ) {
+
+        filein.open( input_filenames[i] );
+
+        if (!filein.is_open()) {
+          std::cerr << "Error opening the input file " << input_filenames[i] << "." << std::endl;
+          return 1;
+        }
+
+        std::cout << "Input file " << input_filenames[i] << " is read and points are saved..." << std::endl;
+
+        while(!filein.eof()) {
+          filein >> x_value >> y_value >> z_value;
+          point = {x_value, y_value, z_value};
+          if ( i == 0 )
+            reference_frames.add_point_first_vector(point);
+          else
+            reference_frames.add_point_second_vector(point);
+        }
+
+        filein.close();
+      }
+
+      if ( !reference_frames.same_number_of_points() ) {
+        std::cout << "warning: The two sets of points have different size. The additional points will be neglected." << std::endl;
+      }
+
     }
     else {
       std::cerr << "No input files are provided. Provide two files with the sets of coordinates of the 3D points." << std::endl;
       return 1;
     }
 
+    //  ----------------------------------- Set the algorithm to compute the rotation -----------------------------------
+    std::unique_ptr<lsq::ComputeRotation> algorithm;
+
     if ( algorithm_strategy == std::string("svd") ) {
       std::cout << "The chosen algorithm is svd." << std::endl;
+      algorithm = std::unique_ptr<lsq::ComputeRotation>( new lsq::SVDMethod() );
     }
     else if ( algorithm_strategy == std::string("quat") ) {
       std::cout << "The chosen algorithm is quat." << std::endl;
+      algorithm = std::unique_ptr<lsq::ComputeRotation>( new lsq::QuaternionMethod() );
     }
     else {
       std::cerr << "The chosen method is not available. Refer to the --help for the available methods." << std::endl;
       return 1;
     }
 
-    std::cout << "Here it goes the implementation of the application." << std::endl;
+    reference_frames.set_rotation_strategy( std::move(algorithm) );
+
+    // ---------------------- Compute the centroids and the update the sets of points around them -----------------------
+    reference_frames.centroid_first_vector();
+    reference_frames.centroid_second_vector();
+    reference_frames.update_first_points_around_centroid();
+    reference_frames.update_second_points_around_centroid();
+
+    // ---------------------------------------------- Compute the H matrix ----------------------------------------------
+    reference_frames.compute_H_matrix();
+
+    // ---------------------------------------- Compute rotation and translation ----------------------------------------
+    reference_frames.compute_rotation_matrix();
+    reference_frames.compute_translation_vector();
+
+    // ----------------------------------------- Print rotation and translation -----------------------------------------
+    Eigen::Matrix3d rotation = reference_frames.get_rotation_matrix();
+    Eigen::Vector3d translation = reference_frames.get_translation_vector();
+
+    std::cout << "The rotation matrix is:" << std::endl;
+    std::cout << rotation << std::endl;
+    std::cout << "The translation vector is:" << std::endl;
+    std::cout << translation << std::endl;
 
     }
     catch(std::exception& e)
